070403f508f99a2e6f6b4400eb0ca671dfcf8ea2
Mark Farman <farman@email.uky.edu>
2026-04-08 16:11:30 -0400

    Modify SLURM settings and enhance script documentation
    Updated SLURM job configuration and improved script comments for clarity. Adjusted resource allocation and file handling for assembly process.

--- scripts/trim-spades.sh
+++ scripts/trim-spades.sh
@@ -1,53 +1,78 @@
 #!/bin/bash
-# runs trimmomatic then spades assembler
+
+# Change partition info to match configuration
 
 #SBATCH --time 48:00:00
-#SBATCH --job-name=trim-spades
+#SBATCH --job-name=trim-spades-mask
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=8
-#SBATCH --partition=normal
-#SBATCH --mem=500GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --partition=short
+#SBATCH --mem=128GB
 #SBATCH --mail-type ALL
-#SBATCH -A coa_farman_uksr
-#SBATCH --mail-type ALL
-#SBATCH --mail-user farman@uky.edu
+#SBATCH --mail-user user@domain
 
 echo "SLURM_NODELIST: "$SLURM_NODELIST
 
 # Requires paired-end read datasets
 
-#1. Place raw reads into RAW_READS directory
+# 1. Place raw reads into a directory
+# e.g. cp strainID_1.fq.gz; strainID_2.fq.gz RAW_READS
 
-#2. Run assembly using command: $ sbatch trim-spades.sh RAW_READS SRRXXXXXXX yes
+# 2. Run assembly using command: $ sbatch trim-spades.sh <reads-directory> <reads-prefix>
+# e.g. sbatch trim-spades.sh RAW_READS strainID
 
-#3. Use for loop to submit assemblies in parallel: $ for f in `cat SRRlist.txt`; do sbatch trim-spades.sh RAW_READS $f yes; done
+# 3. Use for loop to submit assemblies in parallel
+# e.g. for prefix in `cat reads-prefix-list.txt`; do sbatch trim-spades.sh RAW_READS $prefix; done
 
+readsdir=$1
 
-dir=$1
+prefix=$2
 
-f=$2
+mkdir $prefix 
 
-mkdir $f 
+shopt -s extglob
 
-cp $dir/$f*_1*f*q* $f/
+cp "$readsdir/${prefix}"*_1.@(fq|fastq|fq.gz|fastq.gz) $prefix/
 
-cp $dir/$f*_2*f*q* $f/
+cp "$readsdir/${prefix}"*_2.@(fq|fastq|fq.gz|fastq.gz) $prefix/
 
-cd $f
+cd $prefix
 
 if [ $3 == 'yes' ]
 then
   singularity run --app trimmomatic039 /share/singularity/images/ccs/conda/amd-conda2-centos8.sinf trimmomatic PE \
-  -threads 16 -phred33 -trimlog ${f}_errorlog.txt \
-  $f*_1*.f*q* $f*_2*.f*q* \
-  ${f}_R1_paired.fq ${f}_R1_unpaired.fq \
-  ${f}_R2_paired.fq ${f}_R2_unpaired.fq \
-  ILLUMINACLIP:/project/farman_uksr/adapters/NexteraPE-PE.fa:2:30:10 SLIDINGWINDOW:20:20 MINLEN:90;    # change path to fasta file of adaptor sequences as appropriate
+  -threads 32 -phred33 \
+  "${prefix}"*_1.@(fq|fastq|fq.gz|fastq.gz) "${prefix}"*_2.@(fq|fastq|fq.gz|fastq.gz) \
+  ${prefix}_R1_paired.fq.gz ${prefix}_R1_unpaired.fq.gz \
+  ${prefix}_R2_paired.fq.gz ${prefix}_R2_unpaired.fq.gz \
+  ILLUMINACLIP:/project/farman_uksr/adapters/NexteraPE-PE.fa:2:30:10 SLIDINGWINDOW:20:20 MINLEN:90;
 fi
+
+rm ${prefix}_R1_unpaired.fq.gz
+rm ${prefix}_R2_unpaired.fq.gz
 
 # now run spades
 
-singularity run --app spades3155 /share/singularity/images/ccs/conda/amd-conda9-rocky8.sinf spades.py --pe1-1 ${f}_R1_paired.fq --pe1-2 ${f}_R2_paired.fq --pe1-s ${f}_R1_unpaired.fq --pe1-s ${f}_R2_unpaired.fq -o ${f}_assembly
+singularity run --app spades3155 /share/singularity/images/ccs/conda/amd-conda9-rocky8.sinf \
+  spades.py --pe1-1 ${prefix}_R1_paired.fq.gz --pe1-2 ${prefix}_R2_paired.fq.gz \
+  -o ${prefix}_assembly
 
-# values for --app and path to trimmomatic/spades imags will need to be updated for your system
+
+# following version uses unpaired read data also - usually produces lower quality assembly
+
+#singularity run --app spades3155 /share/singularity/images/ccs/conda/amd-conda9-rocky8.sinf \
+#  spades.py --pe1-1 ${prefix}_R1_paired.fq.gz --pe1-2 ${prefix}_R2_paired.fq.gz \
+#  --pe1-s ${prefix}_R1_unpaired.fq --pe1-s ${prefix}_R2_unpaired.fq \
+#  -o ${prefix}_assembly
+
+# add genome ID to contig-level assembly filename
+mv ${prefix}_assembly/contigs.fasta ${prefix}_assembly/${prefix}.fasta
+
+# add genome ID to scaffold-level assembly filename
+mv ${prefix}_assembly/scaffolds.fasta ${prefix}_assembly/${prefix}_scaffolds.fasta
+
+perl /project/farman/SCRIPTs/GenomeProcessFull.pl ${prefix}_assembly/${prefix}_scaffolds.fasta
+
+# singularity calls and path to trimmomatic/spades images will need to be updated for your system
+
